5f06b746e1d2f14715c08d4e15c0e71b2ced95a2
scapellera <scapellera@ctw.es>
2017-02-22 18:21:27 +0100

    reorganizacion parte 2

--- web/buscador.php
+++ web/buscador.php
@@ -1,98 +1,39 @@
-    <!doctype html>
-
 <?php
 session_start();
-include('../assets/php/db.php');
-if($_SESSION["login_done"]==true){
+include('assets/php/db.php');
+if ($_SESSION["login_done"] == true){
 ?>
 
 
 <html lang="en">
 <head>
-	<meta charset="utf-8" />
-	<link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta charset="utf-8"/>
+    <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 
-	<title>WEB TEST</title>
+    <title>WEB TEST</title>
 
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
-
-
-    <!-- ARCHIVOS NECESARIOS PARA DATATABLES-->
-<script src="https://code.jquery.com/jquery-1.12.3.js"></script>
-
-<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-
-<script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
-
-<!--<link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">-->
-
-     <!-- ARCHIVOS NECESARIOS PARA DATATABLES-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
-
-    <!--COLUMNAS QUE PUEDEN SER MODIFICADAS-->
-    <script type="text/javascript" src="../assets/js/editor.js"></script>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <meta name="viewport" content="width=device-width"/>
 
 
-    <!-- DATATABLES TABLAS -->
-    <script src="../table/tables.js"></script>
-    <!-- Bootstrap core CSS     -->
-    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-
-    <!-- Animation library for notifications   -->
-    <link href="../assets/css/animate.min.css" rel="stylesheet"/>
-
-    <!--  Light Bootstrap Table core CSS    -->
-    <link href="../assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
-
-
-    <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="../assets/css/demo.css" rel="stylesheet" />
-
-
-    <!--     Fonts and icons     -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-    <link href="../assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
-
-    <!-- nuestro css -->
-    <link href="../assets/css/micss.css" rel="stylesheet" />
-    
-    <!--<script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>-->
-    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-    <!--EDIT DATATABLE CODE TYPE TABLE-->
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet"/>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/js/bootstrap-editable.min.js"></script>
-    
-    <!--TABLE_EDITOR.CSS-->
-    <link href="../assets/css/table_editor.css" rel="stylesheet"/>
-    <link href="../assets/css/table.css" rel="stylesheet"/>
-    <!--BOTONES EXCEL-->
-    <script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
-    <script src="http://cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
-    <script src="http://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
-    <script src="http://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
-    <script src="http://cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
-    <script src="http://cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
-    <!--BOTONES EXCEL CSS-->
-    <link href="../assets/css/table2.css" rel="stylesheet"/>
-    <!--EDIT DATATABLE CODE-->
-    <link href="../assets/css/table4.css" rel="stylesheet"/>
-    <!--INSERTS-->
-    <link href="../assets/css/insert.css" rel="stylesheet" />
-
+    <?php include('assets/librerias/librerias_globales.html'); ?>
 
 
 </head>
 <body>
 <div class="wrapper">
     <div class="sidebar">
-    	<div class="sidebar-wrapper">
-        <!--AQUI VA EL MENU-->
-        <?php include('../assets/menu/menu_principal.html');?>
+        <div class="sidebar-wrapper">
+            <!--MENU Y LOGO-->
+            <?php
+            include('assets/html/logo/logo.html');
+            include('assets/html/menu/menu_principal.html');
+            ?>
+            <!--CAMBIAR COLOR DE LA ENTRADA DE MENU ACTIVA-->
+            <script>$(function () {
+                    document.getElementById("menu_buscador").className = "active";
+                });</script>
         </div>
     </div>
 
@@ -100,25 +41,12 @@ if($_SESSION["login_done"]==true){
         <nav class="navbar navbar-default navbar-fixed">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                    <!--TITULO DE LA PÁGINA-->
                     <a class="navbar-brand">Buscador</a>
                 </div>
                 <div class="collapse navbar-collapse">
-
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="../user.php"> <?php echo $_SESSION["username"]; ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a>
-                        </li>
-                        <li>
-                            <a href="../../logout.php">Log out
-                            </a>
-                        </li>
-                    </ul>
+                    <!--USER & LOGOUT-->
+                    <?php include('assets/html/menu/user_logout.html'); ?>
                 </div>
             </div>
         </nav>
@@ -130,16 +58,15 @@ if($_SESSION["login_done"]==true){
                     <div class="col-md-12">
                         <div class="card">
 
-                               <a href="buscador_clientes.php" class="button">Buscar cliente</a>
-                               <a href="buscador_sedes.php" class="button">Buscar <br> sede</a>
-                               <a href="buscador_contactos.php" class="button">Buscar contacto</a>
-                               <a href="buscador_mayoristas.php" class="button">Buscar mayorista</a>
-                               <a href="buscador_usuarios.php" class="button">Buscar usuario</a>
-                               <a href="buscador_servicios.php" class="button">Buscar servicio</a>
-                               <a href="buscador_articulos.php" class="button">Buscar artículo</a>
-                               <a href="buscador_stock.php" class="button">Buscar stock</a>
-                               <a href="buscador_calendario.php" class="button">Calendario</a>
-                               <a href="buscador_minutajes.php" class="button">Minutaje</a>
+                            <a href="./buscador/buscador_clientes.php" class="button">Buscar cliente</a>
+                            <a href="./buscador/buscador_sedes.php" class="button">Buscar <br> sede</a>
+                            <a href="./buscador/buscador_contactos.php" class="button">Buscar contacto</a>
+                            <a href="./buscador/buscador_mayoristas.php" class="button">Buscar mayorista</a>
+                            <a href="./buscador/buscador_usuarios.php" class="button">Buscar usuario</a>
+                            <a href="./buscador/buscador_servicios.php" class="button">Buscar servicio</a>
+                            <a href="./buscador/buscador_articulos.php" class="button">Buscar artículo</a>
+                            <a href="./buscador/buscador_stock.php" class="button">Buscar stock</a>
+                            <a href="./buscador/buscador_minutaje.php" class="button">Buscar minutaje</a>
 
                         </div>
                     </div>
@@ -148,39 +75,12 @@ if($_SESSION["login_done"]==true){
         </div>
     </div>
 </div>
-
-
 </body>
-
-    <!--   Core JS Files   -->
-    <!--<script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>-->
-	<script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
-
-	<!--  Checkbox, Radio & Switch Plugins -->
-	<script src="../assets/js/bootstrap-checkbox-radio-switch.js"></script>
-
-	<!--  Charts Plugin -->
-	<script src="../assets/js/chartist.min.js"></script>
-
-    <!--  Notifications Plugin    -->
-    <script src="../assets/js/bootstrap-notify.js"></script>
-
-    <!--  Google Maps Plugin    -->
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
-
-    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
-	<script src="../assets/js/light-bootstrap-dashboard.js"></script>
-
-	<!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
-	<script src="../assets/js/demo.js"></script>
-
-
 </html>
 
-<?php 
-}else{
+<?php
+} else {
     echo "false";
     header("location:../index.php");
 }
-
 ?>
--- web/index.php
+++ web/index.php
@@ -1,97 +1,41 @@
-    <!doctype html>
+<!doctype html>
 
 <?php
 session_start();
 include('assets/php/db.php');
-if($_SESSION["login_done"]==true){
+if ($_SESSION["login_done"] == true){
 ?>
 
 
 <html lang="en">
 <head>
-	<meta charset="utf-8" />
-	<link rel="icon" type="image/png" href="assets/img/favicon.ico">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta charset="utf-8"/>
+    <link rel="icon" type="image/png" href="assets/img/favicon.ico">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 
-	<title>CTW - intranet</title>
+    <title>CTW - intranet</title>
 
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <meta name="viewport" content="width=device-width"/>
 
-
-    <!-- ARCHIVOS NECESARIOS PARA DATATABLES-->
-<script src="https://code.jquery.com/jquery-1.12.3.js"></script>
-
-<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-
-<script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
-
-<!--<link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">-->
-
-     <!-- ARCHIVOS NECESARIOS PARA DATATABLES-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
-
-
-    <!-- DATATABLES TABLAS -->
-    <script src="assets/table/tables.js"></script>
-    <!-- Bootstrap core CSS     -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-
-    <!-- Animation library for notifications   -->
-    <link href="assets/css/animate.min.css" rel="stylesheet"/>
-
-    <!--  Light Bootstrap Table core CSS    -->
-    <link href="assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
-
-
-    <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="assets/css/demo.css" rel="stylesheet" />
-
-
-    <!--     Fonts and icons     -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
-
+    <?php include('assets/librerias/librerias_globales.html'); ?>
     
-    <!--<script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>-->
-    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-    <!--EDIT DATATABLE CODE TYPE TABLE-->
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet"/>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/js/bootstrap-editable.min.js"></script>
-    
-    <!--TABLE_EDITOR.CSS-->
-    <link href="assets/css/table_editor.css" rel="stylesheet"/>
-    <link href="assets/css/table.css" rel="stylesheet"/>
-    <!--BOTONES EXCEL-->
-    <script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
-    <script src="http://cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
-    <script src="http://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
-    <script src="http://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
-    <script src="http://cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
-    <script src="http://cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
-    <!--BOTONES EXCEL CSS-->
-    <link href="assets/css/table2.css" rel="stylesheet"/>
-    <!--EDIT DATATABLE CODE-->
-    <link href="assets/css/table4.css" rel="stylesheet"/>
-
-
 
 </head>
 <body>
 <div class="wrapper">
     <div class="sidebar">
-    	<div class="sidebar-wrapper">
+        <div class="sidebar-wrapper">
             <!--MENU Y LOGO-->
             <?php
             include('assets/html/logo/logo.html');
             include('assets/html/menu/menu_principal.html');
             ?>
             <!--CAMBIAR COLOR DE LA ENTRADA DE MENU ACTIVA-->
-            <script>$(function(){document.getElementById("menu_inicio").className = "active";});</script>
-    	</div>
+            <script>$(function () {
+                    document.getElementById("menu_inicio").className = "active";
+                });</script>
+        </div>
     </div>
 
     <div class="main-panel">
@@ -102,8 +46,8 @@ if($_SESSION["login_done"]==true){
                     <a class="navbar-brand">Página de inicio</a>
                 </div>
                 <div class="collapse navbar-collapse">
-                <!--USER & LOGOUT-->
-                <?php include('assets/html/menu/user_logout.html');?>
+                    <!--USER & LOGOUT-->
+                    <?php include('assets/html/menu/user_logout.html'); ?>
                 </div>
             </div>
         </nav>
@@ -113,7 +57,7 @@ if($_SESSION["login_done"]==true){
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card">
-                        <!--CONTENIDO VA AQUI-->
+                            <!--CONTENIDO VA AQUI-->
                         </div>
                     </div>
                 </div>
@@ -127,8 +71,8 @@ if($_SESSION["login_done"]==true){
 </body>
 </html>
 
-<?php 
-}else{
+<?php
+} else {
     echo "false";
     header("location:../index.php");
 }
