53accfcc81d0da81cc1f0d75a7ba664bce6dbc57
Sergi Capellera <scapellera@ctw.es>
2017-08-28 11:55:47 +0200

    crear tabla pdf

--- web/index2.php
+++ web/index2.php
@@ -1,16 +1,25 @@
 <?php ob_start(); ?>
     <head>
-    <style>
-
-    </style>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <style>
+        .datos_cliente{
+            font-size: 10px;
+        }
+        </style>
     </head>
     <body>
     <div id="factura">
         <div id="cabecera_factura">
-            <div id="logo">
-
+            <div id="logo" style="margin-left: 10%;margin-top: 10%">
+                <img src="assets/img/logo_factura.jpg" alt="CTW Logo" style="width: 80%">
             </div>
-            <div id="datos_cliente">
+            <div id="datos_cliente" style="margin-left: 60%" >
+                <a class="datos_cliente"<b>LIU·JO SUCURSAL EN ESPAÑA</b></a><br>
+                <a class="datos_cliente">NIF - W0055163J</a><br>
+                <a class="datos_cliente">NIF intra - ESW0055163J</a><br>
+                <a class="datos_cliente">Domicilio Paseo de Gracià, num.51 Local 102</a><br>
+                <a class="datos_cliente">Boulevard Rosa</a><br>
+                <a class="datos_cliente">08007 - Barcelona</a><br>
 
             </div>
 
@@ -29,7 +38,7 @@ $dompdf = new DOMPDF();
 $dompdf->load_html(ob_get_clean());
 $dompdf->render();
 $pdf = $dompdf->output();
-$filename = "./factura_pdf/ejemplo".time().'.pdf';
-file_put_contents($filename, $pdf);
-/*$dompdf->stream($filename);*/
+$filename = "./factura_pdf/ejemplo" . time() . '.pdf';
+/*file_put_contents($filename, $pdf);*/
+$dompdf->stream($filename);
 ?>
