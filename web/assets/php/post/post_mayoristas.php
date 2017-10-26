6eea4a88d4e4e78e3fdbd9d2575afbb52b782d91
scapellera <scapellera@ctw.es>
2017-10-26 14:15:21 +0200

    cosas

--- web/assets/php/post/post_mayoristas.php
+++ web/assets/php/post/post_mayoristas.php
@@ -1,4 +1,4 @@
-    <!doctype html>
+<!doctype html>
 
 <?php
 session_start();
@@ -10,7 +10,6 @@ if($_SESSION["login_done"]==true){
 
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
 
     <!--INSERTS-->
     <link href="../../css/insert.css" rel="stylesheet" />
@@ -22,72 +21,62 @@ if($_SESSION["login_done"]==true){
 
 <div>
 
-					<?php
-					//Declaramos las variables del formulario
+    <?php
+    //Declaramos las variables del formulario
 
-					$NIF_mayorista = $_POST['NIF_mayorista'];
-					$nombre_empresa = $_POST['nombre_empresa'];
-					$nombre_comercial = $_POST['nombre_comercial'];
-					$telefono_empresa = $_POST['telefono_empresa'];
-					$telefono_comercial = $_POST['telefono_comercial'];
-					$extension_telefono_comercial = $_POST['extension_telefono_comercial'];
-					$email_empresa = $_POST['email_empresa'];
-					$email_comercial = $_POST['email_comercial'];
-                    $ubicacion = $_POST['ubicacion'];
-					$pais = $_POST['select_box_pais'];
-					$prefijo = select_prefijo_pais($pais);
-                    $activo = $_POST['activo'];
-					$IBAN = $_POST['IBAN'];
+    $NIF_mayorista = $_POST['NIF_mayorista'];
+    $nombre_empresa = $_POST['nombre_empresa'];
+    $nombre_comercial = $_POST['nombre_comercial'];
+    $telefono_empresa = $_POST['telefono_empresa'];
+    $telefono_comercial = $_POST['telefono_comercial'];
+    $extension_telefono_comercial = $_POST['extension_telefono_comercial'];
+    $email_empresa = $_POST['email_empresa'];
+    $email_comercial = $_POST['email_comercial'];
+    $ubicacion = $_POST['ubicacion'];
+    $activo = $_POST['activo'];
+    $IBAN = $_POST['IBAN'];
 
+    $pais = utf8_encode($_POST['select_box_pais']);
 
-                    if($activo==''){
-                        $activo2 = 0;
-                    }else{
-                        $activo2 = 1;
-                    }                    
+    $prefijo = select_prefijo_pais($pais);
 
-					//Añadimos comillas a los varchars
-					$NIF_mayorista="\"$NIF_mayorista\"";
-					$nombre_empresa="\"$nombre_empresa\"";
-					$nombre_comercial="\"$nombre_comercial\"";
-					$email_empresa="\"$email_empresa\"";
-					$email_comercial="\"$email_comercial\"";
-                    $ubicacion="\"$ubicacion\"";
-					$pais="\"$pais\"";
-					$IBAN="\"$IBAN\"";
+    $pais2 = $_POST['select_box_pais'];
 
+    $activo2=1;
 
-					//Si hay algun campo opcional no rellenado lo transforma en null
-					if($nombre_comercial == "\"\""){
-						$nombre_comercial = 'null';
-					}if($telefono_comercial == null){
-						$telefono_comercial ='null';
-					}if($extension_telefono_comercial == null){
-						$extension_telefono_comercial ='null';
-					}if($email_comercial == "\"\""){
-						$email_comercial ='null';
-					}if($ubicacion == "\"\""){
-                        $ubicacion ='null';
-                    }if($IBAN == "\"\""){
-						$IBAN ='null';
-					}if($email_empresa == "\"\""){
-                        $email_empresa ='null';
-                    }
+    //Si hay algun campo opcional no rellenado lo transforma en null
+    if($nombre_comercial == "" )){
+        $nombre_comercial = null;
+    }if($telefono_comercial == ""){
+        $telefono_comercial =null;
+    }if($extension_telefono_comercial == ""){
+        $extension_telefono_comercial =null;
+    }if($email_comercial == ""){
+        $email_comercial =null;
+    }if($ubicacion == ""){
+        $ubicacion =null;
+    }if($IBAN == ""){
+        $IBAN =null;
+    }if($email_empresa == ""){
+        $email_empresa =null;
+    }
 
 
 					//Conectamos con la base de datos, hacemos los inserts y cerramos conexion.
 					$conn = connect();
 
 					$sql = "INSERT INTO MAYORISTA (NIF_MAYORISTA, nombre_empresa, nombre_comercial, telefono_empresa, telefono_comercial, extension_telefono_comercial, email_empresa, email_comercial, ubicacion, pais, prefijo, IBAN,activo)
-					VALUES ($NIF_mayorista, $nombre_empresa, $nombre_comercial, $telefono_empresa, $telefono_comercial, $extension_telefono_comercial, $email_empresa, $email_comercial, $ubicacion, $pais, $prefijo, $IBAN,$activo2)";
-					    
+					VALUES ('$NIF_mayorista', '$nombre_empresa','$nombre_comercial', $telefono_empresa, $telefono_comercial, '$extension_telefono_comercial', '$email_empresa', '$email_comercial', '$ubicacion', '$pais2', $prefijo, '$IBAN','$activo2')";
+
 
 
 					if ($conn->query($sql) === TRUE) {
                         header('Location: ../../../insert.php?ok=altaMayorista');
-					} else {
-                        header('Location: ../../../insert.php?ok=altaMayorista');
-					}
+                    } else {
+                        /*header('Location: ../../../insert.php?error=altaMayorista');*/
+
+                        echo $conn->error;
+                    }
 
 					close($conn); 
 					 
@@ -98,7 +87,7 @@ if($_SESSION["login_done"]==true){
 </body>
 </html>
 
-<?php 
+<?php
 }else{
     echo "false";
     header("location:../index.php");
